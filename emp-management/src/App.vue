2a17d91fba91ae5ebee1fe749a5a43b1f6b90649
akash <akash.ju.msc@gmail.com>
2026-09-14 10:54:47 +0600

    feat(tailwind): configured tailwindcss

--- emp-management/src/App.vue
+++ emp-management/src/App.vue
@@ -2,7 +2,7 @@
 
 <template>
   <h1>You did it!</h1>
-  <p>
+  <p class="tracking-tight line-through">
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
