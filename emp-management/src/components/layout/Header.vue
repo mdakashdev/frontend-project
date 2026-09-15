f744463ad97f21fb962c25b6eda9b22a7fade98e
akash <akash.ju.msc@gmail.com>
2026-09-15 10:09:17 +0600

    feat(header): designed header part

--- emp-management/src/components/layout/Header.vue
+++ emp-management/src/components/layout/Header.vue
@@ -1,11 +1,31 @@
+<script setup lang="ts">
+import {
+  Search,
+  Users,
+  ChevronDown,
+  Bell
+} from 'lucide-vue-next'
+</script>
 <template>
-  <header class="h-16 border-b flex items-center justify-between px-6">
-    <div class="font-semibold">
-      Admin Dashboard
+  <header class="flex items-center h-16 border-b border-slate-200 justify-between px-6">
+    <div class="relative w-120">
+      <Search
+        :size="18"
+        class="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
+      />
+      <input
+        type="search"
+        placeholder="Search anything"
+        class="w-full rounded-md border border-slate-200 bg-background py-2 pl-10 pr-3 outline-none focus:ring-2 focus:ring-blue-500"
+        required
+      />
     </div>
 
-    <div class="text-sm text-muted-foreground">
-      User
+    <div class="flex gap-3 items-center">
+      <Bell :size="20" class="mr-4" />
+      <Users :size="20" />
+      <span>Mr. Users</span>
+      <ChevronDown :size="20" />
     </div>
   </header>
 </template>
