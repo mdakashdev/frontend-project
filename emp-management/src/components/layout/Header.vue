843b355e87db259e1332607649a1856cc1edff35
akash <akash.ju.msc@gmail.com>
2026-09-15 12:35:19 +0600

    fix(header): implement input field from shadcn

--- emp-management/src/components/layout/Header.vue
+++ emp-management/src/components/layout/Header.vue
@@ -5,6 +5,8 @@ import {
   ChevronDown,
   Bell
 } from 'lucide-vue-next'
+
+import { Input } from '@/components/ui/input'
 </script>
 <template>
   <header class="flex items-center h-16 border-b border-slate-200 justify-between px-6">
@@ -13,12 +15,16 @@ import {
         :size="18"
         class="absolute left-3 top-1/2 -translate-y-1/2 text-muted-foreground"
       />
-      <input
-        type="search"
-        placeholder="Search anything"
-        class="w-full rounded-md border border-slate-200 bg-background py-2 pl-10 pr-3 outline-none focus:ring-2 focus:ring-blue-500"
-        required
-      />
+<!--      <input-->
+<!--        type="search"-->
+<!--        placeholder="Search anything"-->
+<!--        class="w-full rounded-md border border-slate-200 bg-background py-2 pl-10 pr-3 outline-none focus:ring-2 focus:ring-blue-500"-->
+<!--        required-->
+<!--      />-->
+    <Input
+      placeholder="Search anything"
+      class="pl-10"
+    />
     </div>
 
     <div class="flex gap-3 items-center">
